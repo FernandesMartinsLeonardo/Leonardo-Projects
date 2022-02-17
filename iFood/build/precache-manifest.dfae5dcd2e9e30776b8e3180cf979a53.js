1600f916e88541edfcdcaa1378f8245014b91945
Leonardo Martins <fmartins.leonardo@gmail.com>
2022-02-17 15:16:19 -0300

    projeto instagram

---
+++ iFood/build/precache-manifest.dfae5dcd2e9e30776b8e3180cf979a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22bcdbb56642e3504e028c0215aff4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f904ee7403fdd350c71f",
+    "url": "/static/js/2.d6b3931a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d6b3931a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "407c310b036531a551b9",
+    "url": "/static/js/main.e752b7ec.chunk.js"
+  },
+  {
+    "revision": "5b89fbb19c832785a0b6",
+    "url": "/static/js/runtime-main.37baf46b.js"
+  },
+  {
+    "revision": "38d98590d73ab828b738069336b4ae3c",
+    "url": "/static/media/errorPage.38d98590.png"
+  }
+]);
